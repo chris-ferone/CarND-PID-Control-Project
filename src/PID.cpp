37b51cfb812c8b73bb1433462c6c1f55438d12b1
Chris Ferone <chrisferone@gmail.com>
2017-08-08 21:26:31 -0400

    Tested Single P controller. Works for a few seconds but then becomes unstable

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,24 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	Kp=0;
+	Ki=0;
+	Kd=0;
+	p_error=0;
+    i_error=0;
+    d_error=0;
+	
 }
 
 void PID::UpdateError(double cte) {
+	
+	p_error = Kp * cte;
+	i_error += Ki* cte;
+	d_error += Kd * cte;	
 }
 
 double PID::TotalError() {
+	return p_error + i_error + d_error;
+	
 }
 
